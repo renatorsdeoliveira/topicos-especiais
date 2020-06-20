e7e48d216f08bc051664515c90a0c7d89a32fb88
Renato_O <renatorsdeoliveira>
2020-06-20 17:44:11 -0300

    Traduzido o projeto

--- backend/src/main/java/net/guides/springboot2/springboot2jpacrudexample/Application.java
+++ backend/src/main/java/net/guides/springboot2/springboot2jpacrudexample/Application.java
@@ -24,24 +24,24 @@ public class Application implements CommandLineRunner {
     @Override
     public void run(String...args) throws Exception {
 
-        // Create an employee
-        Employee employee = new Employee();
-        employee.setFirstName("Ramesh");
-        employee.setLastName("Fadatare");
-        employee.setEmailId("ramesh@gmail.com");
-        employeeRepository.save(employee);
-        // Create an employee
-        Employee employee1 = new Employee();
-        employee1.setFirstName("Tom");
-        employee1.setLastName("Cruise");
-        employee1.setEmailId("tom@gmail.com");
-        employeeRepository.save(employee1);
-     // Create an employee
-        Employee employee2 = new Employee();
-        employee2.setFirstName("Tony");
-        employee2.setLastName("Stark");
-        employee2.setEmailId("tony@gmail.com");
-        employeeRepository.save(employee2);
+//        // Create an employee
+//        Employee employee = new Employee();
+//        employee.setFirstName("Ramesh");
+//        employee.setLastName("Fadatare");
+//        employee.setEmailId("ramesh@gmail.com");
+//        employeeRepository.save(employee);
+//        // Create an employee
+//        Employee employee1 = new Employee();
+//        employee1.setFirstName("Tom");
+//        employee1.setLastName("Cruise");
+//        employee1.setEmailId("tom@gmail.com");
+//        employeeRepository.save(employee1);
+//     // Create an employee
+//        Employee employee2 = new Employee();
+//        employee2.setFirstName("Tony");
+//        employee2.setLastName("Stark");
+//        employee2.setEmailId("tony@gmail.com");
+//        employeeRepository.save(employee2);
     }
 
     public static void main(String[] args) {
