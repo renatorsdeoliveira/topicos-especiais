4bfa417f4e6a1a7ac85ff89fb80bf6b3dae08585
Renato_O <renatorsdeoliveira>
2020-06-21 11:50:22 -0300

    Atualizado a rota

--- frontend/src/app/employee.service.ts
+++ frontend/src/app/employee.service.ts
@@ -7,7 +7,7 @@ import { Observable } from 'rxjs';
 })
 export class EmployeeService {
 
-  private baseUrl = 'http://localhost:8080/springboot-crud-rest/api/v1/employees';
+  private baseUrl = 'http://localhost:8080/springboot-crud-rest/api/v1/empregados';
 
   constructor(private http: HttpClient) { }
 
